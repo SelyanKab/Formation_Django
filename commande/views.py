347b2db80db7a996d0c9df156c37ea4aec1a4e36
SelyanKab <selyan.kab@protonmail.com>
2021-03-25 21:44:16 +0100

    Merge remote-tracking branch 'origin/master'

--- commande/views.py
+++ commande/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+# Create your views here
+def list_commande(request):
+    return HttpResponse('la liste des commandes')
