37c27c5fb7305738f4302f7c56beb385d3f8cff2
SelyanKab <selyan.kab@protonmail.com>
2021-03-26 14:50:33 +0100

    Abeddel n useɣwen deg views n produit s ufaylu amager.html

--- produit/views.py
+++ produit/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse('Azul fell-awen·akent')
+    return render(request,'produit/amager.html')
