2b85ffa13a587a8c5f68be41bed9dc60da64ee5e
Selyan Slimane Amiri <selyan.kab@protonmail.com>
2021-03-27 09:45:07 +0100

    Timerna n ulugu

--- projet/settings.py
+++ projet/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'projet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/images/'
