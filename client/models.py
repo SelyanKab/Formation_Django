3d19820c31d76bcb2ac6c1a37e7d6b53cb80c81f
Selyan Slimane Amiri <selyan.kab@protonmail.com>
2021-03-28 12:55:15 +0200

    Aseɣti n Tmerna n models

--- client/models.py
+++ client/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-    nom = mpdels.CharField(max_length=200, null=True)
+    nom = models.CharField(max_length=200, null=True)
     telephone = models.CharField(max_length=200, null=True)
     date_creation = models.DateTimeField(auto_now_add=True, null=True)
--- commande/models.py
+++ commande/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Commande(models.Model):
-    STATUS = (('en instance','en instance'),
-            ('non livré','non livré'),
-              ('livré','livré'))
-    #client =
-    #produit =
-    status =models.CharField(max_length=200, numll=True,choices=SATUS)
+    STATUS = (('en instance', 'en instance'),
+              ('non livré', 'non livré'),
+              ('livré', 'livré'))
+    # client =
+    # produit =
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_creation = models.DateTimeField(auto_now_add=True, null=True)
