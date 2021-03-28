f075a0c80ec96d78adf3daf95a06d7613e0b48e5
Selyan Slimane Amiri <selyan.kab@protonmail.com>
2021-03-28 12:48:58 +0200

    Timerna n models

--- client/models.py
+++ client/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    nom = mpdels.CharField(max_length=200, null=True)
+    telephone = models.CharField(max_length=200, null=True)
+    date_creation = models.DateTimeField(auto_now_add=True, null=True)
--- produit/models.py
+++ produit/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Produit(models.Model):
+    nom = models.CharField(max_length=200, null=True)
+    prix = models.FloatField(null=True)
+    date_creation = models.DateTimeField(auto_now_add=True, null=True)
